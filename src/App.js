b6fc102183c3f1d600f7ccb5585c558f7c2cfeb9
pallavi1812 <upallavi1812@gmail.com>
2021-05-02 23:59:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+import useStyles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const classes = useStyles();
+
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    return (
+        <>
+            <CssBaseline>
+                <AppBar position="relative">
+                    <Toolbar>
+                        <PhotoCamera className={classes.icon} />
+                        <Typography variant="h6" >Photo Album</Typography>
+                    </Toolbar>
+                </AppBar>
+                <main>
+                    <div>
+                        <Container maxWidth="sm" className={classes.container}>
+                            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                                Photo Album
+                           </Typography>
+                            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           </Typography>
+                            <div>
+                                <Grid container spacing={2} justify="center">
+                                    <Grid item>
+                                        <Button variant="contained" color="primary" className={classes.button}>
+                                            See Photos
+                                       </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="outlined" color="primary" className={classes.button}>
+                                            Secondary Action
+                                       </Button>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Container>
+                        <Container className={classes.cardGrid} maxWidth="md">
+                            <Grid container spacing={4}>
+                                {array.map((card) => (
+                                    <Grid item key={card} xs={12} sm={6} md={4}>
+                                        <Card className={classes.card}>
+                                            <CardMedia
+                                                className={classes.cardMedia}
+                                                image="https://source.unsplash.com/random/"
+                                                title="Image Title"
+                                            />
+                                            <CardContent className={classes.CardContent}>
+                                                <Typography gutterBottom variant="h3">
+                                                    Heading
+                                    </Typography>
+                                                <Typography >
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button size="small" color="primary">
+                                                    View
+                                            </Button>
+                                                <Button size="small" color="primary">
+                                                    Edit
+                                            </Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Container>
+                        <footer className={classes.footer}>
+                            <Typography variant="h6" gutterBottom align="center">
+                                Footer
+                            </Typography>
+                            <Typography variant="subtitle1" color="textSecondary" align="center">
+                                Footer's Subtitle
+                            </Typography>
+                        </footer>
+                    </div>
+                </main>
+            </CssBaseline>
+        </>
+    );
 }
 
 export default App;
